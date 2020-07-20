cc4dddbceb4cd55b6b036ac36198a42b8057c381
Ibrahem Youssef <67711816+Ibrahemyoussef2020@users.noreply.github.com>
2020-07-20 22:45:51 +0200

    Add files via upload

---
+++ get-your-templete/get-your-templete.js
@@ -0,0 +1,32 @@
+function Dontgiveup(){
+	 document.getElementById("onLab").style.display="flex"; 
+
+     document.getElementById("Dontgiveup").innerHTML = "dblClick" 
+}
+
+
+
+
+
+function Dontgiveup2(){
+	 document.getElementById("onLab").style.display="none";
+	 document.getElementById("Dontgiveup").innerHTML = "Don`t give up"  
+
+}
+
+
+function edite(){
+	 document.getElementById("edite").style.transform="translateX(.5%)";
+	 document.getElementById("btn-edite").innerHTML = "dblClick";
+	 
+
+}
+
+function edite2(){
+	 document.getElementById("edite").style.transform="translateX(103%)";
+	 document.getElementById("btn-edite").innerHTML = "more";  
+
+}
+
+
+
